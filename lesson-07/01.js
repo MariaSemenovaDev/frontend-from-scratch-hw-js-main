4e50c29a3faf4d3128966bf01127807e03b31f04
Maria Semenova <semenovamk96@mail.ru>
2024-11-26 14:06:32 +0300

    7lesson - 1task

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -7,55 +7,12 @@
 */
 
 function capitalizeWords(str) {
-
-
-
-
+  const array = str.split(" ")// дал нам массив слов
+for (let i = 0; i < array.length; i++) {
+    array[i] = array[i][0].toUpperCase() + array[i].slice(1)
+}
+return array
 }
 
+console.log(capitalizeWords("hello world from javascript"));
 
-
-// console.log(capitalizeWords("hello world from javascript"));
-
-
-// const array = str.split(" ")// дал нам массив строк
-// // // return array
-// const newArray = []
-// for (let i = 0; i < array.length; i++) {
-//   const word = array[i]; //берем каждое слово
-//   const firstWord = word[0].toUpperCase() + word.slice(1)
-//   newArray.push(firstWord)
-// }
-// return newArray
-// }
-
-
-
-  // return str.toUpperCase()
-
-
-// const array =[str]
-// return array
-
-// let str = "hello world from javascript"
-
-
-// const array = [...str]
-// console.log(array);
-
-
-// const array = str.split(" ")//дал нам массив строк
-// for (let i = 0; i < array.length; i++) {
-
-//   const element = array[i];
-//   const firstWord = element[0].toUpperCase()
-//   // element.splice(0, firstWord)
-//   const www = element.splice(0, 'firstWord') // + [...element]); //подобрались к каждой первой букве
-//   console.log(www);
-  
-  
-//   console.log(element);//выдал номер каждого элемента 
-//   console.log(array.indexOf(element));//выдал номер каждого элемента 
-   
-  
-// }
